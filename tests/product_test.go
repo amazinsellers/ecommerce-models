b081a8c871fabf82e083334ab9d8cd4664233529
Premkumar Anand <amazinsellers@gmail.com>
2023-07-19 19:01:04 +0100

    add tests for product conversion

---
+++ tests/product_test.go
@@ -0,0 +1,29 @@
+package tests
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/amazinsellers/ecommerce-models/amazon"
+	"io/ioutil"
+	"testing"
+)
+
+func TestProduct(t *testing.T) {
+	file, err := ioutil.ReadFile("./amazon-product.json")
+	if err != nil {
+		t.Error("amazon-product.json file not readable")
+	}
+
+	jsonContent := string(file)
+
+	productArr := &amazon.ProductArray{}
+	err = productArr.Unmarshal(jsonContent)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	marshal, _ := json.Marshal(productArr)
+	fmt.Println(string(marshal))
+}
